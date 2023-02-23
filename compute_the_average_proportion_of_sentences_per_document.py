40bae34f8aa92e8b9ed5608e1e9be923b8d44f68
SAID Raoufdine <raoufdine.said@etu.univ-lyon1.fr>
2023-02-23 11:27:48 +0100

    Program who compute the average proportion and the average number of sentences per document added

---
+++ compute_the_average_proportion_of_sentences_per_document.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Compute the average proportion of sentences per document
+def compute_the_average_proportion_of_sentences_per_document(df):
+    sum_proportion = 0
+    for idx in df.index:
+        sum_proportion += sum(df["own_labels"][idx]) / len(df["own_labels"][idx])
+    return sum_proportion / len(df.index)
+
+print(compute_the_average_proportion_of_sentences_per_document(pd.read_json("./data/train.json")))
