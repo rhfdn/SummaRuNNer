0ddcf3e5c3c5e5eef0256d22371b89512cd5a475
SAID Raoufdine <raoufdine.said@etu.univ-lyon1.fr>
2023-02-27 14:58:38 +0100

    Erreur in the extension of the file in the creation

--- best_SummaRuNNer.py
+++
@@ -1,2 +0,0 @@
-The best models for each modification:
- - RNN_RNN_parallelising_word_GRU: Epoch 5 : train loss = 0.346, val loss = 0.358, train mae = 0.210, val mae = 0.209, train accuracy = 0.823, val accuracy = 0.818
