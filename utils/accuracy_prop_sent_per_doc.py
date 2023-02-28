5d3fe0f93203ee9e6c99591bf140022eda398ecd
SAID Raoufdine <raoufdine.said@etu.univ-lyon1.fr>
2023-02-28 11:51:40 +0100

    Next step: 5 executions on the labs machine to obtain real score

--- utils/accuracy_prop_sent_per_doc.py
+++ utils/accuracy_prop_sent_per_doc.py
@@ -1,22 +1,9 @@
 import math
 import numpy as np
 
+from .threshold_probs import threshold_probs_by_prop
+
 # Compute the accuracy
 def accuracy_prop_sent_per_doc_fn(probs, targets, doc_lens, average_proportion_of_sentences_per_document=0.2670278281534701):
-    result = []
-    doc_i = 0
-    doc_len = 0
-    doc_line_i = 0
-    while doc_i < len(doc_lens):
-        doc_len = doc_lens[doc_i]
-        doc_prob = np.array([probs[i] for i in range(len(probs))])
-        n = math.ceil(average_proportion_of_sentences_per_document * len(doc_prob))
-        for i in range(n):
-            idx = np.argmax(doc_prob)
-            doc_prob[idx] = -1
-        doc_prob[doc_prob >= 0] = 0
-        doc_prob[doc_prob == -1] = 1
-        result = result + [doc_prob[i] for i in range(len(probs))]
-        doc_i += 1
-        doc_line_i += doc_len
+    result = threshold_probs_by_prop(probs, doc_lens, average_proportion_of_sentences_per_document)
     return sum([result[i] == targets[i] for i in range(len(targets))]) / len(targets)
