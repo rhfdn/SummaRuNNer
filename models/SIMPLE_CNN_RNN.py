1031fa8aa4ee5de0b6778c0a6aa41bae97cb9bbd
SAID Raoufdine <said.raoufdine@gmail.com>
2023-03-08 14:56:34 +0100

    SIMPLE_CNN_RNN model code moved to his file

---
+++ models/SIMPLE_CNN_RNN.py
@@ -0,0 +1,107 @@
+from .BasicModel import BasicModel
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class SIMPLE_CNN_RNN(BasicModel):
+    def __init__(self, device, vocab_size, word_embed = None, dim_word_embed = 100):
+        super(SIMPLE_CNN_RNN, self).__init__(device)
+
+        self.device = device
+
+        self.word_embedding = nn.Embedding(vocab_size+2, dim_word_embed, padding_idx=0)
+        # Load word embedding if specified
+        if word_embed is not None:
+            self.word_embedding = torch.nn.Embedding.from_pretrained(torch.from_numpy(word_embed).float())
+
+        # 100 : word2vec embedding size
+        self.word_convs = nn.Sequential(
+                            nn.Conv1d(in_channels=100,out_channels=100,kernel_size=4,padding='same'),
+                            nn.BatchNorm1d(num_features=100),
+                            nn.ReLU(inplace=True))
+        self.sent_GRU = nn.GRU(input_size = 100, hidden_size=200, batch_first = True, bidirectional = True)
+
+        # 10: relative position range size, with segment size = 10
+        self.rel_pos_emb = nn.Embedding(11, 100)
+        self.abs_pos_emb = nn.Embedding(100, 100)
+
+        self.Wdoc = nn.Linear(2*200,2*200,bias=True)
+
+        self.Wcontent = nn.Linear(2*200,1,bias=False)
+        self.Wsalience = nn.Bilinear(2*200,2*200,1,bias=False)
+        self.Wnovelty = nn.Bilinear(2*200,2*200,1,bias=False)
+        self.Wabs_pos = nn.Linear(100,1,bias=False)
+        self.Wrel_pos = nn.Linear(100,1,bias=False)
+        self.bias = nn.Parameter(torch.empty(1).uniform_(-0.1, 0.1))
+
+    def avg_pool1d(self,x,seq_lens):
+        out = []
+        for index,t in enumerate(x):
+            if seq_lens[index] == 0:
+                t = t[:1]
+            else:
+                t = t[:seq_lens[index],:]
+            t = torch.t(t).unsqueeze(0)
+            out.append(torch.avg_pool1d(t,t.size(2)))
+        
+        out = torch.cat(out).squeeze(2)
+        return out
+
+    def forward(self, arr_x, doc_lens):
+        probs = []
+
+        max_doc_lens = max(doc_lens)
+        #sent_lens = torch.sum(torch.sign(arr_x),dim=1).data
+
+        arr_x = self.word_embedding(arr_x)
+        arr_x = self.word_convs(arr_x.permute(0,2,1))
+        arr_x = torch.avg_pool1d(arr_x, arr_x.size(2)).squeeze(2)
+
+        arr_x = self.pad_doc(arr_x, doc_lens)
+
+        arr_x = self.sent_GRU(arr_x)[0]
+        sents_all_docs = arr_x.reshape(len(doc_lens) * max_doc_lens, 2*200) # flat docs
+        
+        docs = self.avg_pool1d(arr_x, doc_lens)
+        docs = torch.tanh(self.Wdoc(docs))
+        docs_for_each_sent = docs.repeat_interleave(max_doc_lens, dim=0) # repeat doc n times
+        
+        contents = self.Wcontent(sents_all_docs)
+        saliences = self.Wsalience(sents_all_docs, docs_for_each_sent)
+
+        # Compute position embedding
+        abs_poses = torch.tensor([[position] for position in range(max_doc_lens)], dtype=torch.long)
+        abs_poses = abs_poses.to(self.device)
+        abs_poses = self.abs_pos_emb(abs_poses).squeeze(1)
+        aps = self.Wabs_pos(abs_poses)
+
+        # Compute relative position embedding
+        rel_poses = torch.tensor([[int(round(position / 10))] for position in range(max_doc_lens)], dtype=torch.long)
+        rel_poses = rel_poses.to(self.device)
+        rel_poses = self.rel_pos_emb(rel_poses).squeeze(1)
+        rps = self.Wrel_pos(rel_poses)
+
+        # doc_len (number) s
+        ses = torch.zeros(len(doc_lens), 2*200)
+        ses = ses.to(self.device)
+
+        # for each document, compute probabilities
+        for position in range(max_doc_lens):
+            contents_for_position = contents[position::max_doc_lens]
+            saliences_for_position = saliences[position::max_doc_lens]
+            novelties_for_position = -1 * self.Wnovelty(sents_all_docs[position::max_doc_lens], torch.tanh(ses))
+            prob = contents_for_position + saliences_for_position + novelties_for_position + aps[position] + rps[position] + self.bias
+            prob = torch.sigmoid(prob)
+            probs.append(prob)
+
+            ses = ses + torch.mul(sents_all_docs[position::max_doc_lens], prob)
+        probs = torch.cat(probs).squeeze()
+        probs_filtered = []
+        for idx, doc_len in enumerate(doc_lens):
+            prob = probs[idx::len(doc_lens)]
+            prob = prob[:doc_len]
+            probs_filtered.append(prob)
+        probs_filtered = torch.cat(probs_filtered).squeeze()
+        #probs = probs.to(self.device)
+        return probs_filtered
