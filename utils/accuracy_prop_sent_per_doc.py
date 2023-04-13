10673dd0a39c416eb271177341a2a9cb89111ef5
Raoufdine SAID <said.raoufdine@gmail.com>
2023-04-13 19:09:21 +0200

    Bugs fixed

--- utils/accuracy_prop_sent_per_doc.py
+++ utils/accuracy_prop_sent_per_doc.py
@@ -4,6 +4,6 @@ import numpy as np
 from .threshold_probs import threshold_probs_by_prop
 
 # Compute the accuracy
-def accuracy_prop_sent_per_doc_fn(probs, targets, doc_lens, average_proportion_of_sentences_per_document=0.2670278281534701):
+def accuracy_prop_sent_per_doc_fn(probs, targets, doc_lens, average_proportion_of_sentences_per_document):
     result = threshold_probs_by_prop(probs, doc_lens, average_proportion_of_sentences_per_document)
     return sum([result[i] == targets[i] for i in range(len(targets))]) / len(targets)
