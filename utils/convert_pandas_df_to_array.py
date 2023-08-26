5f4f4a2623e589e8350c2e4dcde869119beff3fe
Raoufdine SAID <said.raoufdine@gmail.com>
2023-08-26 11:06:06 +0200

    optimized

--- utils/convert_pandas_df_to_array.py
+++ utils/convert_pandas_df_to_array.py
@@ -2,5 +2,5 @@
 def convert_pandas_df_to_array(df):
     arr = []
     for i in range(df.shape[0]):
-        arr.append({col : df[col][i] for col in df.columns})
+        arr.append({col : df.iloc[i][col] for col in df.columns})
     return arr
